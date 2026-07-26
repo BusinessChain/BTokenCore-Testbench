0dc0557f2aefca29fe793ac35b5511da8873242d
BusinessChain <BusinessChain@protonmail.com>
2026-07-26 23:31:58 +0200

    backup

--- Testbench.cs
+++ Testbench.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using BTokenLib;
 
 
@@ -14,12 +15,45 @@ class Testbench : ILogEntryNotifier
 
   public void Start()
   {
-    TokenBitcoin tokenBitcoin = new TokenBitcoin(this);
+    Console.WriteLine($"Test 1: Make an instance of TokenBitcoin.");
+    
+    try
+    {
+      TokenBitcoin tokenBitcoin = new TokenBitcoin(this);
 
+      Console.WriteLine($"Test 1 result: success");
+    }
+    catch(Exception ex)
+    {
+      Console.WriteLine($"Test 1 result: fail");
+      Console.WriteLine($"\n{ex.GetType().Name}: {ex.Message}");
+      Console.WriteLine($"\nStack trace:\n");
+
+      PrintStackTrace(ex);
+    }
   }
 
   public void NotifyLogEntry(string logEntry, string source)
   {
     Console.WriteLine($"{source}: {logEntry}");
+  }
+
+  static void PrintStackTrace(Exception ex)
+  {
+    StackTrace stackTrace = new(ex, true);
+
+    foreach (StackFrame frame in stackTrace.GetFrames() ?? [])
+    {
+      var method = frame.GetMethod();
+      var type = method?.DeclaringType;
+
+      // Skip framework code.
+      if (type?.Namespace == null || !type.Namespace.StartsWith("BToken"))
+        continue;
+
+      Console.WriteLine(
+          $"   at {type.FullName}.{method!.Name} " +
+          $"in {frame.GetFileName()}:line {frame.GetFileLineNumber()}");
+    }
   }
 }
