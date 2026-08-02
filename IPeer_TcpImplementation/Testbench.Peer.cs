d8ecb5e75853c983f13b29112e0561e7f5d0d588
BusinessChain <BusinessChain@protonmail.com>
2026-08-02 02:54:28 +0200

    backup

--- IPeer_TcpImplementation/Testbench.Peer.cs
+++ IPeer_TcpImplementation/Testbench.Peer.cs
@@ -12,9 +12,44 @@ namespace BTokenCore_Testbench;
 
 public partial class Testbench
 {
+  // Das Netzwerk holt die Konfiguration vom Token ab.
+  int Port;
+  UInt32 ProtocolVersion = 70015;
+  ulong NetworkServicesLocal = 0;
+  ulong NetworkServicesRemote = 0;
+  string UserAgent = "/BTokenCore:0.0.0/";
+  byte RelayOption = 0x01;
+
   public enum ConnectionType { OUTBOUND, INBOUND };
   List<string> IPAddresses = new();
 
+
+  public async Task<IPeer> GetInterfacePeer()
+  {
+    IPAddress iP = LoadIPAddress();
+
+    TcpClient tcpClient = new TcpClient();
+
+    try
+    {
+      Peer peer = new(CreateStateMachineProtocol(), tcpClient, Peer.ConnectionType.OUTBOUND, iP);
+
+      await peer.Start();
+
+      return peer;
+    }
+    catch (Exception ex)
+    {
+      tcpClient.Dispose();
+      return null;
+    }
+  }
+
+
+  readonly DirectoryInfo DirectoryPeers = Directory.CreateDirectory(
+        Path.Combine(GetType().Name, "logPeers"));
+  readonly DirectoryInfo DirectoryPeersDisposed = Directory.CreateDirectory(
+    Path.Combine(DirectoryPeers.FullName, "disposed"));
 
   IPAddress LoadIPAddress()
   {
@@ -67,27 +102,119 @@ public partial class Testbench
     return iPAddresses.Select(iP => IPAddress.Parse(iP)).ToList();
   }
 
-  public async Task<IPeer> GetInterfacePeer()
+  public List<string> GetSeedAddresses()
   {
-    IPAddress iP = LoadIPAddress();
+    //mit DNS seeds arbeiten.
+    //seed.bitcoin.sipa.be
+    //dnsseed.bluematt.me
+    //dnsseed.bitcoin.dashjr.org
+    //seed.bitcoinstats.com
+    //seed.bitnodes.io
 
-    TcpClient tcpClient = new TcpClient();
+    return new List<string>()
+        {"83.229.86.158" 
+        // 84.74.69.100
+        };
+  }
+
+
+  public async Task StartPeerInboundConnector()
+  {
+    TcpListener tcpListener = new(IPAddress.Any, Port);
 
     try
     {
-      Peer peer = new(CreateStateMachineProtocol(), tcpClient, Peer.ConnectionType.OUTBOUND, iP);
+      Log($"Start TCP listener on port {Port}.");
+      tcpListener.Start(COUNT_MAX_INBOUND_CONNECTIONS);
+    }
+    catch (Exception ex)
+    {
+      Log($"Failed to start TCP listener on port {Port}.\n {ex.Message}");
+      return;
+    }
+
+    while (true)
+    {
+      try
+      {
+        TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+        IPAddress remoteIP = ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address;
+
+        Log($"Received inbound request on port {Port} from {remoteIP}.");
+
+        if (!ValidateInboundPeer(remoteIP))
+        {
+          tcpClient.Dispose();
+          continue;
+        }
+
+        CreatePeerInbound(tcpClient, remoteIP);
+      }
+      catch (Exception ex)
+      {
+        Log($"{ex.GetType().Name} in peer connector background process:\n {ex.Message}");
+
+        await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+      }
+    }
+  }
+
+  bool ValidateInboundPeer(IPAddress remoteIP)
+  {
+    string rejectionString = "";
+
+    lock (LOCK_Peers)
+    {
+      if (Peers.Any(p => p.IPAddress.Equals(remoteIP)))
+        rejectionString = $"Peer {remoteIP} already connected.";
+      else if (Peers.Count(p => p.Connection == ConnectionType.INBOUND) >= COUNT_MAX_INBOUND_CONNECTIONS)
+        rejectionString = $"Max number ({COUNT_MAX_INBOUND_CONNECTIONS}) of inbound connections reached.";
+    }
+
+    if (rejectionString == "")
+    {
+      if (remoteIP.ToString() != "84.74.69.100")
+        rejectionString = $"Peer {remoteIP} not on whitelist.";
+      else
+        foreach (FileInfo iPDisposed in DirectoryPeersDisposed.EnumerateFiles())
+          if (iPDisposed.Name.Contains(remoteIP.ToString()) && iPDisposed.Name.Contains(ConnectionType.INBOUND.ToString()))
+          {
+            int secondsBanned = TIMESPAN_PEER_BANNED_SECONDS -
+              (int)(DateTime.Now - iPDisposed.CreationTime).TotalSeconds;
+
+            if (secondsBanned > 0)
+            {
+              rejectionString = $"{iPDisposed.Name} is banned for {secondsBanned} seconds.";
+              break;
+            }
+          }
+    }
+
+    if (rejectionString != "")
+    {
+      Log($"Inbound peer {remoteIP} rejected: \n{rejectionString}");
+      return false;
+    }
+
+    return true;
+  }
+  async Task CreatePeerInbound(TcpClient tcpClient, IPAddress iP)
+  {
+    try
+    {
+      Peer peer = new(CreateStateMachineProtocol(), tcpClient, C, iP);
 
       await peer.Start();
 
-      return peer;
+      lock (LOCK_Peers)
+        Peers.Add(peer);
     }
     catch (Exception ex)
     {
       tcpClient.Dispose();
-      return null;
     }
   }
-
 
 
   Dictionary<string, MessageNetworkProtocol> CreateStateMachineProtocol()
