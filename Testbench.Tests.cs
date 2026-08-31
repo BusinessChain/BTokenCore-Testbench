3f1798db0b95985b683052896ae33ac34371035a
BusinessChain <BusinessChain@protonmail.com>
2026-08-31 21:47:18 +0200

    backup

--- Testbench.Tests.cs
+++ Testbench.Tests.cs
@@ -114,7 +114,7 @@ internal partial class Testbench
 
     internal override bool TryRun(out string message)
     {
-      Blockchain blockchain = new(Testbench, Testbench.CreateHeaderGenesis());
+      Blockchain blockchain = new(Testbench.CreateHeaderGenesis());
 
       blockchain.LoadFromDisk();
 
